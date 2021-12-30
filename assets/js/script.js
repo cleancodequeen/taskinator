52896838ceda75fbdcc6e7beb32b16cc9325ad03
Clean Code Queen <shanenmsmith@gmail.com>
2021-12-29 22:14:44 -0500

    added div with task heading and subheading

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,12 +3,30 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function (event) {
   event.preventDefault();
-  
 
+  var taskNameInput = document.querySelector("input[name='task-name']").value;
+  var taskTypeInput = document.querySelector("select[name='task-type'").value;
+
+  //create list item
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
-  listItemEl.textContent = "This is a new task.";
+
+  //create div to hold task info and add to list item
+  var taskInfoEl = document.createElement("div");
+  taskInfoEl.className = "task-info";
+  taskInfoEl.innerHTML =
+    "<h3 class='task-name'>" +
+    taskNameInput +
+    "</h3><span class='task-type'>" +
+    taskTypeInput +
+    "</span>";
+
+  //attach the div created to the list item
+  listItemEl.appendChild(taskInfoEl);
+
+  //attach the list item to the unordered list
   tasksToDoEl.appendChild(listItemEl);
+  console.dir(listItemEl);
 };
 
 formEl.addEventListener("submit", createTaskHandler);
