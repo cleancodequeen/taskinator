ec2116f2759fc50b771d3b24e2431b326cb5df1e
Clean Code Queen <shanenmsmith@gmail.com>
2021-12-29 22:48:11 -0500

    fixed bugs in css and with function in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,22 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function (event) {
+var taskFormHandler = function (event) {
   event.preventDefault();
-
   var taskNameInput = document.querySelector("input[name='task-name']").value;
   var taskTypeInput = document.querySelector("select[name='task-type'").value;
 
+  //package up data as an object
+  var taskDataObj = {
+    name: taskNameInput,
+    type: taskTypeInput
+  };
+
+  //send it as an argument to createTaskEl
+  createTaskEl(taskDataObj);
+};
+
+var createTaskEl = function (taskDataObj) {
   //create list item
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
@@ -16,9 +26,9 @@ var createTaskHandler = function (event) {
   taskInfoEl.className = "task-info";
   taskInfoEl.innerHTML =
     "<h3 class='task-name'>" +
-    taskNameInput +
+    taskDataObj.name +
     "</h3><span class='task-type'>" +
-    taskTypeInput +
+    taskDataObj.type +
     "</span>";
 
   //attach the div created to the list item
@@ -26,8 +36,10 @@ var createTaskHandler = function (event) {
 
   //attach the list item to the unordered list
   tasksToDoEl.appendChild(listItemEl);
-  console.dir(listItemEl);
-};
 
-formEl.addEventListener("submit", createTaskHandler);
+}
+
+formEl.addEventListener("submit", taskFormHandler);
+
+//12-30-2021 - Start on lesson 4.2.8 - RescueTime Active: Get to work!!
 
